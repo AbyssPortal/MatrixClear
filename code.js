1d7c43654f36c329bed3d4b378ef5dd121312ba9
yarde <planteraminehut@gmail.com>
2023-07-31 23:55:06 +0300

    actually did what the previosu commit said

--- code.js
+++ code.js
@@ -623,7 +623,7 @@ function drawScore(score, name) {
         clearNameLabel.style.color = "rgb(140, 3, 232)";
     } else {
         scoreCounterLabel.style.color = "black"
-        clearNameLabel.style.color = "rgb(140, 3, 232)";
+        clearNameLabel.style.color = "black";
     }
     scoreCounterLabel.textContent = "Score: " + JSON.stringify(score);
     clearNameLabel.textContent = name;
